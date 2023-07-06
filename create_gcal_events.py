9f8ba0561a7cda995b1e13fb9d6d394797138995
Herman Feng <hyx.feng@outlook.com>
2023-07-06 19:14:10 +1000

    :wrench: Added patch functionality for times

--- create_gcal_events.py
+++ create_gcal_events.py
@@ -8,11 +8,63 @@ from libs.google_cal_client import GoogleCalClient
 ATTENDEES = json.loads(os.environ['RECIPIENTS'])
 URL = os.environ['SCHEDULE_URL']
 
-# create the scraper client and scrape
-dummyScraper = ScraperClient("HTML Scraper", URL)
+def simplify_existing_events(existing_events_list):
+    '''
+    Args:
+        existing_events_list (dict): The existing events list returned by the google_cal_client.list_events() function.
 
-html_content = dummyScraper.get_html()
-game_data = dummyScraper.scrape_events(html_content=html_content, parse_type="ssb")
+    Returns:
+        Dict containining simplified list of start time, url and event id.
+    '''
+    events_simple = {}
+
+    for event in existing_events_list['items']:
+        time = event['start']['dateTime'][:-6]
+        url = event['extendedProperties']['private']['schedule']
+        events_simple.update({time: {"url":url, "id":event["id"]}})
+
+    return events_simple
+
+def generate_patch_details(round_name:str, tip_off:str, finish:str):
+    '''
+    Args:
+        round_name (str): The name of the round, updated just in case.
+        tip_off (str): The new start time for the event.
+        finish (str): The new end time for the event.
+
+    Returns:
+        Dict containing the patch details.
+    '''
+    patch_details = {
+                        'summary': round_name,
+                        'start': {
+                            'dateTime': tip_off,
+                        },
+                        'end': {
+                            'dateTime': finish, 
+                        }
+                    }
+     
+    return patch_details
+
+def check_attendees():
+    '''
+    Args:
+        html_content (str): The html content in string format that needs to be parsed.
+        type (str): The specific parse method.
+        custom_parse (dict): Optional param to speciy custom fields to parse.
+
+    Returns:
+        Private method for parsing differnt HTML pages.
+    '''
+    return "test"
+
+
+# create the scraper client and scrape
+htmlScraper = ScraperClient("Peter Parker", URL)
+
+html_content = htmlScraper.get_html()
+game_data = htmlScraper.scrape_events(html_content=html_content, parse_type="ssb")
 # pprint(game_data)
 
 # create Google Calendar client
@@ -20,17 +72,14 @@ GClient = GoogleCalClient("Cal.Endar", os.environ['GCAL_CLIENT_ID'], os.environ[
 
 # get list of all events
 all_events = GClient.list_events()
+
 if len(all_events['items']) == 0:
     empty = True
 else:
     empty = False
 
     # create a simplified dict of existing events
-    events_simple = {}
-    for event in all_events['items']:
-        time = event['start']['dateTime'][:-6]
-        url = event['extendedProperties']['private']['schedule']
-        events_simple.update({time:url})
+    events_simple = simplify_existing_events(all_events)
 
 # iterate through the scraped game data
 for game in game_data:
@@ -40,41 +89,48 @@ for game in game_data:
     venue = game["location"]
 
     # add all games in events list is empty
-    if empty:
+    if empty: #TODO probably not super efficient to check this every time - maybe move to the start?
         print(f"\n---\n[{round_name}] is a new event - Creating calendar event:")
         new_event = GClient.create_event(event_name=round_name, start_time=tip_off, end_time=finish, attendees=ATTENDEES, location=venue, private_properties={"schedule":URL})
         print(f'Event [{new_event}] created for [{round_name}]')
 
     # the events list is not empty, check which are new/updated and add them
     else:
-        # there is a perfect match for this event - skip
-        if (tip_off in events_simple) and (events_simple[tip_off] == URL):
+        # OPTION 1: there is a perfect match for this event - skip
+        if (tip_off in events_simple) and (events_simple[tip_off]['url'] == URL):
             print(f"\n---\n[{round_name}] already has an existing event - Skipping")
 
-        # there is a partial match for game date but not time - patch
+        # OPTION 2: there is a partial match for game date but not time - patch
         elif any(tip_off[:-9] in date for date in events_simple.keys()):
 
             # get a list of all events that match date only
             matching_dates = [date for date in events_simple if tip_off[:-9] in date]
             same_schedule_found = False
 
+            # loop through events that match date and check associated schedule
             for matched_date in matching_dates:
 
-                # if these events also belong to the same schedule/URL then update the time
-                if events_simple[matched_date] == URL:
+                # if these events also belong to the same schedule/URL then update the round name & time
+                if events_simple[matched_date]['url'] == URL:
                     print(f"\n---\n[{round_name}] has an existing event with a different start time - Updating:")
-                    print("\t\t TO BE DONE HEHE \t\t") #TODO Add Patching
+                    patch_details = generate_patch_details(round_name=round_name, tip_off=tip_off, finish=finish)
+                    p_id = events_simple[matched_date]['id']
+                    patched_event = GClient.patch_event(event_id=p_id, patched_fields=patch_details)
+
+                    print(f'\tEvent for [{round_name}] patched with updated time [{tip_off}]')
                     same_schedule_found = True
                     break
 
-            # otherwise all matches belong to a different schedule - insert
+            # OPTION 3A: otherwise all matches belong to a different schedule - insert
             if not(same_schedule_found):
                 print(f"\n---\n[{round_name}] is a new event - Creating calendar event:")
                 new_event = GClient.create_event(event_name=round_name, start_time=tip_off, end_time=finish, attendees=ATTENDEES, location=venue, private_properties={"schedule":URL})
                 print(f'Event [{new_event}] created for [{round_name}]')
             
-        # there is no match at all - insert
+        # OPTION 3B: there is no match at all - insert
         else:
             print(f"\n---\n[{round_name}] is a new event - Creating calendar event:")
             new_event = GClient.create_event(event_name=round_name, start_time=tip_off, end_time=finish, attendees=ATTENDEES, location=venue, private_properties={"schedule":URL})
             print(f'Event [{new_event}] created for [{round_name}]')
+
+
--- helpers/html_parser.py
+++ helpers/html_parser.py
@@ -1,82 +1,85 @@
 '''
-Used parse different types of HTML pages and extractcing the relevant information.
+Used parse different types of HTML pages and extracting the relevant information.
 '''
 
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 import re
 
-def parse_html_content(html_content:str, parse_type:str, custom_parse:dict={}):
-    '''
-    Public method for parsing html content - will access private methods as required
+class HTMLHelper:
 
-    Args:
-        html_content (str): The html content in string format that needs to be parsed.
-        type (str): The specific parse method.
-        custom_parse (dict): Optional param to speciy custom fields to parse.
+    @staticmethod
+    def parse_html_content(html_content:str, parse_type:str, custom_parse:dict={}):
+        '''
+        Public method for parsing html content - will access private methods as required
 
-    Returns:
-        Private method for parsing differnt HTML pages.
-    '''
-    match parse_type:
-        case "ssb":
-            return _parse_ssb_content(html_content)
-        case "custom":
-            return _parse_custom_content(html_content, custom_parse)
-        case other:
-            print("Invalid type passed in")
+        Args:
+            html_content (str): The html content in string format that needs to be parsed.
+            type (str): The specific parse method.
+            custom_parse (dict): Optional param to speciy custom fields to parse.
 
+        Returns:
+            Private method for parsing differnt HTML pages.
+        '''
+        match parse_type:
+            case "ssb":
+                return HTMLHelper._parse_ssb_content(html_content)
+            case "custom":
+                return HTMLHelper._parse_custom_content(html_content, custom_parse)
+            case other:
+                print("Invalid type passed in")
 
-def _parse_ssb_content(html_content):
-    '''
-    Parse the SSB page for date-time events for each game.
+    @staticmethod
+    def _parse_ssb_content(html_content):
+        '''
+        Parse the SSB page for date-time events for each game.
 
-    Args:
-        html_content (str): The html content in string format that needs to be parsed.
+        Args:
+            html_content (str): The html content in string format that needs to be parsed.
 
-    Returns:
-        Dict containing the time and date for every upcoming game
+        Returns:
+            Dict containing the time and date for every upcoming game
 
-    '''
-    soup = BeautifulSoup(html_content, "html.parser")
-    games = soup.find_all("div", class_="grid")
-    game_schedules = []
+        '''
+        soup = BeautifulSoup(html_content, "html.parser")
+        games = soup.find_all("div", class_="grid")
+        game_schedules = []
 
-    for element in games:
-        #round = element.find("div", class_="cell").find("h5", text="Round").find_next_sibling(text=True).strip()
-        game_round = element.find(string=re.compile("Round")).find_parent().find_next_sibling(text=True).strip()
-        opponent = element.find(string=re.compile("Opponent")).find_next('a').get_text(strip=True)
-        date = element.find(string=re.compile("Date")).find_parent().find_next_sibling(text=True).strip()
-        time = element.find(string=re.compile("Time")).find_parent().find_next_sibling(text=True).strip()
-        location = element.find(string=re.compile("Court")).find_parent().find_next_sibling(text=True).strip()
+        for element in games:
+            #round = element.find("div", class_="cell").find("h5", text="Round").find_next_sibling(text=True).strip()
+            game_round = element.find(string=re.compile("Round")).find_parent().find_next_sibling(text=True).strip()
+            opponent = element.find(string=re.compile("Opponent")).find_next('a').get_text(strip=True)
+            date = element.find(string=re.compile("Date")).find_parent().find_next_sibling(text=True).strip()
+            time = element.find(string=re.compile("Time")).find_parent().find_next_sibling(text=True).strip()
+            location = element.find(string=re.compile("Court")).find_parent().find_next_sibling(text=True).strip()
 
-        game_details = f"{game_round}: {opponent}"
-        game_start = datetime.strptime((f"{date} {time}"), "%d/%m/%Y %I:%M%p") 
-        game_end = game_start + timedelta(hours=1)
-        game_schedules.append(
-            {
-                "round":game_details, 
-                "start":game_start,
-                "end":game_end,
-                "location":location
-            }
-        )
+            game_details = f"{game_round}: {opponent}"
+            game_start = datetime.strptime((f"{date} {time}"), "%d/%m/%Y %I:%M%p") 
+            game_end = game_start + timedelta(hours=1)
+            game_schedules.append(
+                {
+                    "round":game_details, 
+                    "start":game_start,
+                    "end":game_end,
+                    "location":location
+                }
+            )
 
-    return game_schedules
+        return game_schedules
 
+    @staticmethod
+    def _parse_custom_content(html_content:str, custom_parse:dict):
+        '''
+        Parse the SSB page for date-time events for each game.
 
-def _parse_custom_content(html_content:str, custom_parse:dict):
-    '''
-    Parse the SSB page for date-time events for each game.
+        Args:
+            html_content (str): The html content in string format that needs to be parsed.
 
-    Args:
-        html_content (str): The html content in string format that needs to be parsed.
+        Returns:
+            Dict containing the time and date for every upcoming game
 
-    Returns:
-        Dict containing the time and date for every upcoming game
-
-    '''
-    soup = BeautifulSoup(html_content, "html.parser")
-    
-    return print("This hasn't been setup yet")
+        '''
+        soup = BeautifulSoup(html_content, "html.parser")
+        
+        return print("This hasn't been setup yet")
  
