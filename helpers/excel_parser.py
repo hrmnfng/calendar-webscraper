1036b226be907766f734b86dfd5aa1a9717d5e7a
Herman Feng <hyx.feng@outlook.com>
2023-07-23 21:39:59 +1000

    :construction: Working on excel parsing

---
+++ helpers/excel_parser.py
@@ -0,0 +1,126 @@
+'''
+Used parse different Excel or CSV files given a input file and a data format.
+'''
+
+import pandas as pd
+from datetime import datetime 
+import sys
+from pprint import pprint
+
+class ExcelHelper:
+
+    @staticmethod
+    def read_file(file_path:str):
+        
+        if '.csv' in file_path:
+            df = pd.read_csv(file_path)
+        elif '.xlsx' in file_path:
+            df = pd.read_excel(file_path)
+        
+        return df
+
+    @staticmethod
+    def extract_dates(raw_data):
+        
+        # identify how many columns there are / weeks there are:
+        column_names = raw_data.columns.tolist()
+
+        # print(f'"{raw_data.columns[5]}"') "TUEDAY"
+        # print(f'"{raw_data.columns[12]}"') "TUESDAY "
+
+        # print(raw_data.info())
+        possible_days = []
+        for item in column_names:
+            if type(raw_data.loc[0, item]) == datetime:
+                # print(f'{item} - This is a a valid columm')
+                possible_days.append([item, raw_data.loc[0, item]])
+            # else:
+            #     print(f'\t[{item}] - Nope')
+
+        print(f'There are [{len(possible_days)}] dates in this spreadsheet')
+        
+        # identify which row to look at based on name
+        name_match = 'FU, Michele'
+        mask = raw_data['Select Name'].str.contains(name_match, case=False, na=False)
+        indices_with_partial_match = raw_data.index[mask].tolist()
+        if (len(indices_with_partial_match) > 0) and (len(indices_with_partial_match) > 1) :
+            sys.exit('There is more than one match, please provide a more specific match-value')
+        else:
+            index = indices_with_partial_match[0]
+
+        # Verify that we're getting the right rows of data
+        # n = Name
+        # n+1 = Shift
+        # n+2 = Normal
+        # n+3 = Overtime
+        # n+4 = On Call 
+        n = 5
+        event_type = ['Shift', 'Normal', 'Overtime', 'On Call']
+        calendar_events = []
+
+        for i in range(1, n):
+            row = index + i
+            
+            if raw_data.iloc[row, 1] == event_type[(i-1)]:
+                print(f'This is correct - proceed to loop | value = [{raw_data.iloc[row, 1]}]')
+                
+                if event_type[(i-1)] == 'Shift': # we only care about shift right now
+                    schedule = []
+                    assignment = '' # set this outside of the loop, as some assignments span across multiple cells - should use the last available one if possible
+                    
+                    for item in possible_days:
+                        col = item[0]
+                        date = item[1]
+
+                        # update the assignment, if available
+                        print(f'\nAssignment = [{raw_data.loc[(index), col]}] in row [{index}] of column [{col}/{date}] ')
+                        if type(raw_data.loc[(index), col]) == str: # using index because its at the top of the data
+                            assignment = raw_data.loc[(index), col]
+
+                        # try to extract date time, otherwise just paste the raw text and create an all day event
+                        print(f'\tThe "shift" value is [{raw_data.loc[(row), col]}] in row [{row}] of column [{col}/{date}] ')
+                        raw_time = raw_data.loc[row, col]
+                        start_time, end_time = '', ''
+
+                        # split the raw string to get start and end time
+                        try:
+                            split_times = raw_time.split('-') 
+                            start_time = datetime.strptime(split_times[0], '%H%M').time() 
+                            end_time = datetime.strptime(split_times[1], '%H%M').time()
+
+                        # if a time is not provided, make it literally span all day - NEEDS TO BE CAUGHT LATER DOWN THE LINE #TODO
+                        except Exception as e:
+                            print(f'\tOops something went wrong:\n\t> {e}')
+                            start_time = datetime.strptime('0000', '%H%M').time()
+                            end_time = datetime.strptime('2359', '%H%M').time()
+
+                        # create event start and end datetimes
+                        event_start = datetime.combine(date.date(), start_time)
+                        event_end = datetime.combine(date.date(), end_time)
+
+                            # check if, Lord forbid, it goes past 11:59PM and into the next day
+                            #TODO
+
+                        # check for event name before compiling event details
+                        if assignment == '':
+                            event_name = '<No Data Here>'
+                        else:
+                            event_name = assignment
+                        
+                        roster_event = {event_name: {'start':event_start, 'end':event_end}}
+                        
+                        schedule.append(roster_event)
+
+            else:
+                print(f'You done screwed the pooch | value = [{raw_data.iloc[row, 1]}]; looking for [{event_type[i]}]')
+
+        print('-----')
+
+
+# row duplicate row names are done with .1, .2 ... at the end
+    
+if __name__ == "__main__":
+    raw_data = ExcelHelper.read_file('./Term-3-Intern-Resident-Rosters.xlsx')   
+    ExcelHelper.extract_dates(raw_data=raw_data)    
+
+    pprint(raw_data)
